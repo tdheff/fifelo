ee757efb5f6b9b323fefb4f5bf15e9316bc448c6
Tommy Heffernan <tdheff@gmail.com>
2015-07-16 17:25:44 -0400

    sorting backwards

--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,7 @@ var db = require('../db')
 var app = require('../app')
 
 app.get('/', function(req, res) {
-  db.users.find({}).sort({rating:1}).exec(function(err_user, users) {
+  db.users.find({}).sort({rating:-1}).exec(function(err_user, users) {
     db.games.find({}, function(err_games, games) {
       if (err_user || err_games) {
         res.render('500', {message: err});
