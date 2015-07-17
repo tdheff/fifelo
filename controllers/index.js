2988d61ab9083714866db76dc0224d5f8b193bab
Tommy Heffernan <tdheff@gmail.com>
2015-07-17 18:36:28 -0400

    sorts games by date

--- controllers/index.js
+++ controllers/index.js
@@ -3,7 +3,7 @@ var app = require('../app')
 
 app.get('/', function(req, res) {
   db.users.find({}).sort({rating:-1}).exec(function(err_user, users) {
-    db.games.find({}, function(err_games, games) {
+    db.games.find({}).sort({date:-1}).exec(function(err_games, games) {
       if (err_user || err_games) {
         res.render('500', {message: err});
       }
