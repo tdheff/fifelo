295099ac37333e423f03a2b33497742c0e537f0c
Tommy Heffernan <tdheff@gmail.com>
2015-07-21 18:38:43 -0400

    sort by rating

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,13 @@
 var db = require('../db')
 var app = require('../app')
+var _ = require("underscore")
 
 app.get('/', function(req, res) {
   db.users.once("value", function(users) {
     db.games.once("value", function(games) {
       games = games.val() || [];
-      users = users.val() || [];
+      users = (_.sortBy(_.toArray(users.val()), 'rating')).reverse() || [];
+      console.log(users)
       res.render('index', {users: users, games: games});
     }, function (errorObject) {
       console.log("The read failed: " + errorObject.code);
