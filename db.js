02196be6cf028ae11bef442236dd5be9f16a568b
Tommy Heffernan <tdheff@gmail.com>
2015-08-05 16:09:21 -0400

    mongolab url

--- db.js
+++ db.js
@@ -6,7 +6,7 @@ var monk = require("monk");
 if ('development' == app.get('env')) {
     var database = monk("localhost:27017/fifelo");
 } else {
-    var database = monk("localhost:27017/fifelo");
+    var database = monk("heroku_3q3813qs:arbbr66ac07f5hj9kn5uioc19o@ds031183.mongolab.com:31183/heroku_3q3813qs");
 }
 
 var db = {users: database.get("users"), games: database.get("games")}
